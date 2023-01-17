4a0c93c5d0a7eb36060b73171733645e952933a0
hailey lee <hailey@sharetreats.com>
2023-01-17 18:04:19 +0900

    date picker 변경

--- src/components/elements/ElementsDate.vue
+++ src/components/elements/ElementsDate.vue
@@ -1,81 +1,36 @@
 <script>
-import VueCtkDateTimePicker from "vue-ctk-date-time-picker";
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+import Datepicker from 'vue3-datepicker'
 
 export default{
 	components : {
-		VueCtkDateTimePicker,
+		Datepicker
 	},
 	props :{
-		modelValue: [String, Object],
+		modelValue: Date,
 		name: String,
-		placeholder: String,
-		showFormatType : String,
 		error: String,
-		full: Boolean,
-		disabled : Boolean,
-		auto_close : Boolean,
-		noButtonNow : Boolean,
-		noHeader : Boolean,
-		minDate : String,
-		maxDate : String,
-		format : { type: String, default: "YYYYMMDD" },
-		formatted : { type: String, default: "YYYY-MM-DD" },
-		onlyDate: { type: Boolean, default: true },
-		onlyTime: { type: Boolean, default: false },
-		minuteInterval : Number,
-		range : Boolean
-
+        disabled : Boolean
 	},
 	emits : ['update:modelValue', "changeDate"],
 	methods : {
 		
 	},
-	computed:{
-		dateString:{
-			get(){
-				const self = this;
-				if(!self.modelValue){
-					return;
-				}
-				self.$emit('changeDate', self.modelValue );
-				return self.modelValue;
-			},
-			set(value){ 
-				const self = this;
-				self.$emit('update:modelValue', value);
-				return value; 
-			},
-		}
-	}
 }
 
 </script>
 
 <template>
 	<div>
-		<h1 class="text-sm font-semibold text-slate-800">{{ name }}</h1>
+		<h1 v-if="name" class="mb-3 text-sm font-semibold text-slate-800">{{ name }}</h1>
 		<div
 			class="max-w-6xl h-12 bg-white flex transition-all duration-500"
 		>
-			<VueCtkDateTimePicker
-				class="py-1 overflow-visible px-3 pr-0 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md flex-1 outline-0 h-full text-sm cursor-pointer"
-				:class="{ 'border-red-500': error, 'w-96': (!full), 'w-full': full, 'mt-3': name }"
-				:format="format"
-				:formatted="formatted"
-				v-model="dateString"
-				:only-date="onlyDate"
-				:only-time="onlyTime"
-				:noLabel="!placeholder"
-				:disabled="disabled"
-				:label="placeholder"
-				:autoClose="auto_close"
-				:noButtonNow="noButtonNow"
-				:noHeader="noHeader"
-				:minDate="minDate"
-				:maxDate="maxDate"
-				:minuteInterval="minuteInterval"
-				:range="range"
+			<Datepicker
+                inputFormat="MM/dd/yyyy" 
+                class="shadow-sm block w-full sm:text-sm border-gray-300 rounded-md text-md border pr-3 text-right h-8" 
+				v-model="modelValue"
+                @change="$emit('update:modelValue')"
+                :disabled="disabled"
 			/>
 		</div>
 		<p class="mt-3.5 text-xs font-normal text-red-500" v-show="error">*{{ error }}</p>
