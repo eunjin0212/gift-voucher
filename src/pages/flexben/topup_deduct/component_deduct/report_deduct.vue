5876e6007e510dbd60bf554228f60c9c3668ebf6
hailey lee <hailey@sharetreats.com>
2023-03-07 16:20:48 +0900

    report page / top-up , deduct 페이지 분리

---
+++ src/pages/flexben/topup_deduct/component_deduct/report_deduct.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="p-3 max-w-7xl self-end">
+        <ElementsButton
+            :text="'+ Deduct'"
+            :fitContent="true"
+            class="self-end "
+            @clickEvent="goToRegistering()"
+        />
+    </div>
+    <div class="mt-8 p-3 rounded-lg w-full max-w-7xl bg-white shadow-md shadow-gray-200 flex flex-col">
+        <TimeNavigation
+            :outputFormat="'YYYYMMDD'"
+            @updateDate="clickDateButton"
+        />
+        <div class="flex mt-5 gap-2">
+            <ElementsInput
+                v-model="searchOptions.companyName"
+                :width60="true"
+                :height11="true"
+            />
+            <ElementsButton
+                :width32="true"
+                :text="'Search'"
+                @clickEvent="getFlexbenHistoryList()"
+            />
+        </div>
+        <div class="mt-6 overflow-auto shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-300">
+                <thead>
+                    <tr>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm text-gray-900 sm:pl-6">Date</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm text-gray-900">Company</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm text-gray-900">Admin</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm text-gray-900">Type</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm text-gray-900">Points</th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                    <tr v-for="(history, index) in flexbenHistory.list" v-bind:key="index" >
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-900 sm:pl-6"> {{ history.regDate }} </td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-900"> {{ history.companyName }} </td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-900"> {{ history.executerId }} </td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-900"> Deduct </td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-900"> {{ history.mileageVolume }} </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="w-full h-28 flex justify-center items-center">
+            <ElementsPagination
+                v-model="flexbenHistory.page"
+                :totalContent="flexbenHistory.total"
+                :contentsPerPage="flexbenHistory.limit"
+                @clickPage="clickPageButton"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import TimeNavigation from "@/components/main/TimeNavigation.vue"
+import moment from "moment";
+
+export default {
+    mounted(){
+        const self = this;
+        self.getFlexbenHistoryList();
+    },
+    components : {
+        TimeNavigation
+    },
+    methods : {
+        goToRegistering(){
+            location.href="/flexben/topup_deduct/registering?transaction=DEDUCT";
+        },
+        getFlexbenHistoryList( offset=0, afterClickPage = false ){
+            const self = this;
+            const url = self.$api("uri", "get-flexben-history");
+            const { limit } = self.flexbenHistory;
+            self.flexbenHistory.offset = offset;
+            const json_query = { ...self.searchOptions, offset, limit };
+            self.$axios.get( url , { params : { json_query : JSON.stringify(json_query) } })
+                .then((res) => {
+                    self.flexbenHistory.total = res.data.data.count;
+                    self.flexbenHistory.list = res.data.data.list;
+                    if( ! afterClickPage ){
+                        self.flexbenHistory.page = 1;
+                    }
+
+                })
+                .catch( alert )
+        },
+        clickDateButton( startDate, endDate ){
+            const self = this;
+            self.searchOptions.startDate = startDate;
+            self.searchOptions.endDate = endDate;
+            self.getFlexbenHistoryList();
+        },
+        clickPageButton( item ){
+            const self = this;
+            self.getFlexbenHistoryList( item, true )
+        },
+    },
+    data(){
+        return{
+            flexbenHistory : {
+                list : [],
+                total : 0,
+                limit : 10,
+                offset : null,
+                page : 1,
+            },
+            searchOptions : {
+                startDate : moment().startOf("month").format("YYYYMMDD"),
+                endDate : moment().endOf("month").format("YYYYMMDD"),
+                transactionType : "DEDUCT_TO_HRFLEX",
+                companyName : ""
+            },
+        }
+    }
+}
+</script>
