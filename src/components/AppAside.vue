307297ebb497ed996e56504fcb9acbef8a17bef3
hailey lee <hailey@sharetreats.com>
2023-07-31 16:39:50 +0900

    bug fix

--- src/components/AppAside.vue
+++ src/components/AppAside.vue
@@ -34,7 +34,7 @@ let items = reactive([
         href: "/inquiry",
         img: "app-side-3",
         text: "Activate Process",
-        hasPermission : true
+        hasPermission : checkPermissionArray(['ACTIVATE_PROCESS_VIEW', 'ACTIVATE_PROCESS_EDIT'])
     },
     {
         href: "/company/company_list",
