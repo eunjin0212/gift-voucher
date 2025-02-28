3814c73d6ba60118513986b682c8c429bf991c5b
EunJin Kim <eunjin9639@gmail.com>
2025-02-28 22:42:13 +0900

    feat: 새로운 폰트 추가 및 Tailwind CSS 설정 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,59 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "/index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
-    extend: {
-      minWidth: {
-        'half-vw' : '50vw',
-      }
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                main: {
+                    DEFAULT: '#29ABE2', 
+                    light: '#24BCED'
+                },
+                blue: {
+                    50: '#F1FAFE'
+                },
+                gray: {
+                    50: '#F9FAFC',
+                    100: '#F8FAFC',
+                    200: '#F6F6F6',
+                    300: '#F4F5F6',
+                    400: '#FAFAFA',
+                    500: '#EFEFEF',
+                    600: '#E8EBEE',
+                    700: '#E2E8F0',
+                    800: '#DBDCDD',
+                    900: '#2F3440'
+                },
+                red: {
+                    50: '#FFF1F1',
+                    500: '#F43F5E',
+                    600: '#F63B3B'
+                },
+                pink: {
+                    500: '#E2296C'
+                }
+            },
+            fontFamily: {
+                'sans-bold': ["Source Sans Pro Bold", "sans-serif"],
+                'roboto': ["Roboto", "sans-serif"],
+            },
+            screens: {
+                'xs': '390px',
+                'sm': '768px', // mobile
+                'md': '1024px', // tablet
+            },
+            minWidth: {
+                'xs': '280px', // min mobile
+                'md': '1024px', // tablet
+                'half-vw': '50vw',
+            }
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('flowbite/plugin'),
-  ],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin'),
+    ],
 }
